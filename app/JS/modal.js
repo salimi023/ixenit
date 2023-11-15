a86737d8ab25b2e67b738cc9f6caee19a75112c2
salimi069 <hun.saller@Qgmail.com>
2023-11-16 00:28:13 +0100

    Ading validation script to modal

--- app/JS/modal.js
+++ app/JS/modal.js
@@ -11,7 +11,8 @@ $(document).ready(function() {
         var html = '';
         html += '<div class="w3-row w3-margin-top number_container">';
         html += '<div class="w3-twothird">';
-        html += '<input class="w3-input w3-border" name="phone_number" type="text" maxlength="11" required />';
+        html += '<input class="w3-input w3-border valid" name="phone_number" type="text" maxlength="11" />';
+        html += '<span class="alert"></span>';
         html += '</div>';
         html += '<div class="w3-rest"><button class="w3-btn w3-red w3-round w3-margin-left delete_phone_number">Törlés</button></div>';
         html += '</div>';
@@ -26,7 +27,8 @@ $(document).ready(function() {
     // Remove phone number
     $(document).on("click", "button.delete_phone_number", function(e) {
         e.preventDefault();        
-        $(e.target).closest("div.number_container").remove();        
+        $(e.target).closest("div.number_container").remove();
+        $("span#validationStatus").text('');         
     });
 
     // Add new email
@@ -35,7 +37,8 @@ $(document).ready(function() {
         var html = '';
         html += '<div class="w3-row w3-margin-top email_container">';
         html += '<div class="w3-twothird">';
-        html += '<input class="w3-input w3-border" name="email" type="email" required />';
+        html += '<input class="w3-input w3-border valid" name="email" type="email" />';
+        html += '<span class="alert"></span>';
         html += '</div>';
         html += '<div class="w3-rest"><button class="w3-btn w3-red w3-round w3-margin-left delete_email">Törlés</button></div>';
         html += '</div>';
@@ -46,7 +49,25 @@ $(document).ready(function() {
     // Remove email
     $(document).on("click", "button.delete_email", function(e) {
         e.preventDefault();        
-        $(e.target).closest("div.email_container").remove();        
+        $(e.target).closest("div.email_container").remove();
+        $("span#validationStatus").text('');        
+    });
+
+    // Validate temporary address data
+    $(document).on("change", "input.temp", function() {
+        if($(this).val() !== '') {
+            $("input.temp").each(function() {
+                $(this).addClass('valid'); 
+                $("span.temp_title").removeClass('w3-hide');               
+            }); 
+        } else {
+            $("input.temp").each(function() {
+                $(this).val('');
+                $(this).removeClass('valid');
+                $(this).siblings('span.alert').text('');
+                $("span.temp_title").addClass('w3-hide');                 
+            }); 
+        }
     });
 
     // Close modal
