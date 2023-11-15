5a846ce7e8fb8ee8469332b4f624e1af6e906381
salimi069 <hun.saller@Qgmail.com>
2023-11-16 00:27:34 +0100

    Creating add_user frontend script

--- app/JS/add_user.js
+++ app/JS/add_user.js
@@ -1,1 +1,105 @@
-$(document).ready(function() {});
+/**
+ * Add User
+ */
+$(document).ready(function() {
+    $(document).on("click", "input#save_user", function(e) {
+        e.preventDefault();                                       
+        
+        // Name
+        var first_name = $("input#first_name").val();
+        var last_name = $("input#last_name").val();
+
+        // Address
+        if($("input#zip_code").val() !== '') {
+            var zip_code_valid = validate_digits($("input#zip_code").val());      
+            if(!zip_code_valid) {
+                alert("Kérem, adjon meg egy érvényes irányítószámot!");
+                $("span#validationStatus").text("Error");
+            } else {
+                var zip_code = $("input#zip_code").val();
+            }
+        }               
+        
+        var address = $("input#address").val();
+        var city = $("input#city").val();
+        
+        var temp_zip_code = $("input#temp_zip_code").val();
+        if(temp_zip_code !== '') {            
+            var temp_zip_code_test = validate_digits($("input#temp_zip_code").val());
+
+            if(!temp_zip_code_test) {
+                alert("Kérem, adjon meg egy érvényes irányítószámot!");
+                $("span#validationStatus").text("Error");
+            } else {
+                temp_zip_code = $("input#temp_zip_code").val();
+            }
+        }                       
+
+        var temp_address = $("input#temp_address").val();
+        var temp_city = $("input#temp_city").val();
+
+        // Phone
+        var phone_numbers = [];
+
+        $("input[name='phone_number']").each(function() {
+            var phone_number = $(this).val();            
+
+            if(validate_digits(phone_number)) {
+                phone_numbers.push(phone_number);
+            } else {
+                if(phone_number !== '') {
+                    alert("Kérem, adjon meg egy érvényes telefonszámot, vagy törölje a fölösleges mezőt!");
+                    $("span#validationStatus").text("Error");
+                }                               
+            }
+        });
+
+        // E-mail
+        var emails = [];
+
+        $("input[name='email']").each(function() {
+            var email = $(this).val();            
+
+            if(validate_email(email)) {
+                emails.push(email);
+            } else {
+                if(email !== '') {
+                    alert("Kérem, adjon meg egy érvényes e-mail címet, vagy törölje a fölösleges mezőt!");
+                    $("span#validationStatus").text("Error");
+                }                
+            }
+        });                                                               
+
+        if($("span#validationStatus").text() === '') {
+            
+            // Form data object
+            var customer_data = {
+                first_name: first_name,
+                last_name: last_name,
+                zip_code: zip_code,
+                address: address,
+                city: city,
+                temp_zip_code: temp_zip_code,
+                temp_address: temp_address,
+                temp_city: temp_city,
+                phone: phone_numbers,
+                email: emails            
+            }
+
+            var customer_data_json = JSON.stringify(customer_data);
+
+            console.log(customer_data_json);
+        }               
+    });    
+});
+
+// Checking zip_code datatype
+function validate_digits(code) {
+    return /^\d+$/.test(code);
+}
+
+// Validate email
+function validate_email(email) {
+    var rem = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return rem.test(email);
+} 
