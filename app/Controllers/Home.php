80abb13eb7f94bf4bf8a5cc55592371955422885
salimi069 <hun.saller@Qgmail.com>
2023-11-17 22:09:06 +0100

    Adding view_user function code and constructor

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,6 +2,10 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
 class Home extends BaseController
 {
     const SAVE_NAME_ERROR = 'Sajnos nem sikerült elmenteni a nevet. Kérem, próbálja meg újra!';
@@ -10,41 +14,63 @@ class Home extends BaseController
     const SAVE_EMAIL_ERROR = 'Sajnos nem sikerült elmenteni az e-mail címet. Kérem, próbálja meg újra!';
     const ZIP_CODE_ERROR = 'Az állandó lakcím irányítószáma nem megfelelő. Kérem, ellenőrizze.';
     const TEMP_ZIP_CODE_ERROR = 'Az ideiglenes lakcím irányítószáma nem megfelelő. Kérem, ellenőrizze.';
-    const SAVE_SUCCESS = 'Sikeres mentés.';
-    const SAVE_ERROR = 'Sikertelen mentés.';          
+    const SAVE_SUCCESS = 'success';
+    const SAVE_ERROR = 'Sikertelen mentés.';
+    
+    // DB Models
+    private $user_model = false;
+    private $user_address_model = false;
+    private $user_contact_model = false;
+    
+    // DB Connection
+    private $db = false;
+
+    // Constructor
+    public function initController(
+        RequestInterface $request,
+        ResponseInterface $response,
+        LoggerInterface $logger
+    ) 
+    {
+        parent::initController($request, $response, $logger);
+        $this->user_model = new \App\Models\UserModel;
+        $this->user_address_model = new \App\Models\AddressModel;
+        $this->user_contact_model = new \App\Models\ContactModel;
+        $this->db = \Config\Database::connect('default');
+    }
     
     // Homepage
     public function index(): string
-    {
-        return view('home');
+    {               
+        // List of saved users        
+        $users = $this->user_model->findAll();
+        $users_data['users'] = [];
+        
+        foreach($users as $user) {
+            $users_data['users'][] = $user;
+        }                
+        
+        return view('home', $users_data);
     }
 
     // Add User
     public function add_user() 
-    {               
-        /** DB Connection */
-        $db = \Config\Database::connect('default');
-        
-        /** DB Models */        
-        $user_model = new \App\Models\UserModel;
-        $address_model = new \App\Models\AddressModel;
-        $contact_model = new \App\Models\ContactModel;        
-        
+    {                                                 
         if(isset($_POST)) {
             $user_data = json_decode($_POST['user'], true);
 
             /** Starting transaction */            
-            $db->transStart();
+            $this->db->transStart();
             
             /** Saving user name */            
             $user_name = [
                 'firstname' => $user_data['first_name'],
                 'lastname' => $user_data['last_name']
             ];
-
-            $save_user = $user_model->insert($user_name, false);                                         
+            
+            $save_user = $this->user_model->insert($user_name, false);                                         
             $this->errors($save_user, 'name');                      
-            $saved_user_id = $user_model->getInsertId(); // ID of saved user
+            $saved_user_id = $this->user_model->getInsertId(); // ID of saved user
 
             /** Saving User Address(es) */            
             
@@ -67,7 +93,7 @@ class Home extends BaseController
                 'temp_city' => $user_data['temp_city']                
             ];
 
-            $save_address = $address_model->insert($user_address, false);                        
+            $save_address = $this->user_address_model->insert($user_address, false);                        
             $this->errors($save_address, 'address');                       
             
             /** Saving Phone Number(s) */            
@@ -80,7 +106,7 @@ class Home extends BaseController
                         'contact_type' => 1
                     ];
 
-                    $save_phone = $contact_model->insert($phone_data, false);                               
+                    $save_phone = $this->user_contact_model->insert($phone_data, false);                               
                     $this->errors($save_phone, 'phone');
                 } else {
                     $this->errors(false, 'phone');
@@ -99,7 +125,7 @@ class Home extends BaseController
                         'contact_type' => 2
                     ];
 
-                    $save_email = $contact_model->insert($email_data, false);                                        
+                    $save_email = $this->user_contact_model->insert($email_data, false);                                        
                     $this->errors($save_email, 'email');                    
                 } else {
                     $this->errors($save_email, 'email');                                       
@@ -107,15 +133,53 @@ class Home extends BaseController
             }
             
             /** Completing transaction */            
-            $db->transComplete();
+            $this->db->transComplete();
 
             /** Returning result */            
-            if ($db->transStatus() === false) {
-                $this->errors($db->transStatus(), 'transaction');
+            if ($this->db->transStatus() === false) {
+                $this->errors($this->db->transStatus(), 'transaction');
             } else {
                 echo self::SAVE_SUCCESS;                 
             }                                                      
         }
+    }
+
+    // View User
+    public function view_user() 
+    {
+        if(isset($_POST)) {
+            $user_id = $_POST['id'];            
+
+            if(!empty($user_id)) {                                                                                         
+                echo json_encode($this->get_user_by_id($user_id));                                                                    
+            } else {
+                echo 'user_id_error';
+            }
+        }
+    }
+
+    // Get user data by id
+    private function get_user_by_id($user_id)
+    {
+        $user_data = [];
+
+        // Name
+        $name_data = $this->user_model->where('user_id', $user_id)->findAll();
+        $user_data['name'] = $name_data[0]['lastname'] . ' ' . $name_data[0]['firstname'];
+
+        // Address
+        $address_data = $this->user_address_model->where('user_id', $user_id)->findAll();
+        $user_data['address'] = $address_data[0];
+                
+        // Phone numbers
+        $phone_numbers = $this->user_contact_model->where(['user_id' => $user_id, 'contact_type' => 1])->findAll();
+        $user_data['phone'] = $phone_numbers;
+
+        // Emails
+        $emails = $this->user_contact_model->where(['user_id' => $user_id, 'contact_type' => 2])->findAll();
+        $user_data['email'] = $emails;                
+                                            
+        return $user_data;
     }
 
     // Error handling
@@ -135,5 +199,5 @@ class Home extends BaseController
             echo $message;
             die();
         }
-    }
+    }    
 }
