88f10c1e47913ce9536a8e4ef88c737f8f64587d
salimi069 <hun.saller@Qgmail.com>
2023-11-18 00:41:37 +0100

    Adding new routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,4 +7,6 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->post('home/add_user', 'Home::add_user');
-$routes->post('home/view_user', 'Home::view_user');
+$routes->post('home/view_user', 'Home::get_user_by_id');
+$routes->post('home/get_update_user', 'Home::get_user_by_id');
+$routes->post('home/update_user', 'Home::update_user');
