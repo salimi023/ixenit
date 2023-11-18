f0461f54d17ead1d967dd3391f003dc0a13c5afa
salimi069 <hun.saller@Qgmail.com>
2023-11-18 11:57:36 +0100

    Adding delete_user route and comments

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->post('home/add_user', 'Home::add_user');
-$routes->post('home/view_user', 'Home::get_user_by_id');
-$routes->post('home/get_update_user', 'Home::get_user_by_id');
-$routes->post('home/update_user', 'Home::update_user');
+$routes->get('/', 'Home::index');                                   // Index
+$routes->post('home/add_user', 'Home::add_user');                   // Add User
+$routes->post('home/view_user', 'Home::get_user_by_id');            // Retrieve user data by ID for viewing user data
+$routes->post('home/get_update_user', 'Home::get_user_by_id');      // retrieve user data by ID for updating user data
+$routes->post('home/update_user', 'Home::update_user');             // Update user
+$routes->post('home/delete_user', 'Home::delete_user');             // Delete user
